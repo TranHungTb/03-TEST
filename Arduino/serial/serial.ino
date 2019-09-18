a6c84198458205456a805515211e396486148c97
tran.hung <hung.tran@gfd.com.vn>
2019-09-18 17:58:46 +0700

    game

--- Arduino/serial/serial.ino
+++ Arduino/serial/serial.ino
@@ -1,38 +1,17 @@
-// Basic serial communication with ESP8266
-// Uses serial monitor for communication with ESP8266
-//
-//  Pins
-//  Arduino pin 2 (RX) to ESP8266 TX
-//  Arduino pin 3 to voltage divider then to ESP8266 RX
-//  Connect GND from the Arduiono to GND on the ESP8266
-//  Pull ESP8266 CH_PD HIGH
-//
-// When a command is entered in to the serial monitor on the computer 
-// the Arduino will relay it to the ESP8266
-//
- 
-#include <SoftwareSerial.h>
-SoftwareSerial ESPserial(2,3); // RX | TX
- 
-void setup() 
-{
-    Serial.begin(9600);     // communication with the host computer
-    //while (!Serial)   { ; }
- 
-    // Start the software serial for communication with the ESP8266
-    ESPserial.begin(9600);  
- 
-    Serial.println("");
-    Serial.println("Bắt đầu gửi lệnh.");
-    Serial.println("Ready");
-    Serial.println("");    
+void setup()   {   
+    Serial.begin(9600);
+    pinMode(A3,INPUT_PULLUP); 
+    pinMode(A2,INPUT_PULLUP); 
+    pinMode(A1,INPUT_PULLUP);       
+    pinMode(A0,INPUT_PULLUP); 
 }
- 
-void loop() 
-{
-    // listen for communication from the ESP8266 and then write it to the serial monitor
-    if ( Serial.available() )   {  ESPserial.write( Serial.read() );  }
- 
-    // listen for user input and send it to the ESP8266
-    if ( ESPserial.available() )       {  Serial.write( ESPserial.read() );  }
+int x=60;//hoành độ khảo sát
+byte button;
+void loop(){
+    if (button==3) {
+        x--;// sang trái
+    } else if(button==1) {
+        x++;//sang phải
+    }
+    Serial.println(x);
 }
--- Arduino/sketch_aug30a/sketch_aug30a.ino
+++ Arduino/sketch_aug30a/sketch_aug30a.ino
@@ -1,32 +1,101 @@
-#include <IRremote.h>
-IRsend irsend;// gửi tín hiệu hồng hoại
-const int CH1 = 5;// nút số 1
-const int CH2 = 8;// nút số 2
-const int CH3 = 9;// nút số 3
-const int POWER = 10;// nút Power
+//
+
+
+static const unsigned char PROGMEM  Boss[] =
+{ 
+0XFC,0X00,0XFC,0X00,0XFC,0X00,0X1F,0X80,0X1F,0X80,0X1F,0X80,0XFC,0X00,0XFC,0X00,
+0XFC,0X00
+};
+static const unsigned char PROGMEM  Player[] =
+{ 
+  0XFC,0X00,0XFC,0X00,0XFC,0X00,0XFF,0X80,0XFF,0X80,0XFF,0X80,0XFC,0X00,0XFC,0X00,
+0XFC,0X00
+};
+static const unsigned char PROGMEM  Player2[] =
+{ 
+0XFE,0X00,0XAA,0X00,0XFE,0X40,0X93,0XC0,0XFB,0XC0,0XFB,0XC0,0X92,0X40,0XFE,0X00,
+0XAA,0X00,0XFE,0X00
+};
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>         //download here: https://www.electronoobs.com/eng_arduino_Adafruit_GFX.php
+#include <Adafruit_SSD1306.h>     //downlaod here: https://www.electronoobs.com/eng_arduino_Adafruit_SSD1306.php
+//#define OLED_RESET 5
+Adafruit_SSD1306 display(128, 64);
+//Adafruit_SSD1306 display(OLED_RESET);
+//#define NUMFLAKES 10
+//#define XPOS 0
+//#define YPOS 1
+//#define DELTAY 2
+
+const int left = 2;// nút số 1
+const int up = 3;// nút số 2
+const int right = 4;// nút số 3
+const int down = 5;// nút số 4
+const int POWER = 6;// nút Power
+int led = 13;
+uint8_t x = 0;
+uint8_t y = 0;
 void setup()
 {
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32 or 64 from eBay)
+  delay(100);
+  display.clearDisplay();
+  display.display();
+  delay(100);
+
+  
   Serial.begin(9600);
   /* điện trở nội kéo lên cho tất cả nút bấm*/
-  pinMode(CH1, INPUT_PULLUP);
-  pinMode(CH2, INPUT_PULLUP);
-  pinMode(CH3, INPUT_PULLUP);
+  pinMode(left, INPUT_PULLUP);
+  pinMode(up, INPUT_PULLUP);
+  pinMode(right, INPUT_PULLUP);
+  pinMode(down, INPUT_PULLUP);
   pinMode(POWER, INPUT_PULLUP);
+  
+  pinMode(led, OUTPUT);
 }
 
 void loop()
-{
-  if (digitalRead(CH1) == 1)// khi nút RED được nhấn
+{ 
+  
+  display.clearDisplay();
+  
+  display.drawBitmap( x,y, Player2, 10, 10, 1);          
+  display.display();//Finally display the created image
+
+  if (digitalRead(left) == 0)// khi nút RED được nhấn
   {
-    
+   x--;
+   if(x<1){x=128;}
+   delay(50);
     Serial.println("ra ánh sáng.");
-    irsend.sendSony(0x807FC03F, 32);// gửi tín hiệu HEX 0x10, 12 bits
+    digitalWrite(led,1);
   }
-  else if (digitalRead(CH2) == 0)// khi nút YELLOW được nhấn
-    irsend.sendSony(0x810, 12);// gửi tín hiệu HEX 0x810, 12 bits
-  else if (digitalRead(CH3) == 0)// khi nút GREEN được nhấn
-    irsend.sendSony(0x410, 12);// gửi tín hiệu HEX 0x410, 12 bits
-  else if (digitalRead(POWER) == 0)// khi nút POWER được nhấn
-    irsend.sendSony(0xA90, 12);// gửi tín hiệu HEX 0xA90, 12 bits
+  if (digitalRead(right) == 0)
+  {
+  x++; 
+  if(x>128){x=0;}
+  digitalWrite(led,0);
   delay(50);
+}
+
+ if (digitalRead(up) == 0)// khi nút RED được nhấn
+  {
+   y--;
+   if(y<1){y=64;}
+   delay(50);
+    Serial.println("ra ánh sáng.");
+    digitalWrite(led,1);
+  }
+  if (digitalRead(down) == 0)
+  {
+  y++; 
+  if(y>64){y=0;}
+  digitalWrite(led,0);
+  delay(50);
+}
+
 }
